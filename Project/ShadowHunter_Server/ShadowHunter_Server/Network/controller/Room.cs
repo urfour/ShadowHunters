4429e00af8d057b9a2ea3c140cdee30f553f957d
Romaric Mathis <romaric.mathis@etu.unistra.fr>
2020-02-17 13:22:03 +0100

    Implémentation de la couche réseau du serveur et du client

---
+++ Project/ShadowHunter_Server/ShadowHunter_Server/Network/controller/Room.cs
@@ -0,0 +1,33 @@
+﻿using Network.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network.controller
+{
+    public class Room
+    {
+        private List<Client> Clients { get; set; } = new List<Client>();
+
+        public void BroadCast(Client sender, string data)
+        {
+            foreach(Client c in Clients)
+            {
+                if (c != sender)
+                    c.Send(data);
+            }
+        }
+
+        public void ClientJoin(Client c)
+        {
+            this.Clients.Add(c);
+        }
+
+        public void ClientLeave(Client c)
+        {
+            this.Clients.Remove(c);
+        }
+    }
+}
