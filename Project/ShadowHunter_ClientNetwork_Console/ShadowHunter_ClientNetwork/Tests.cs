a125da9e5c0f28ed3fbabc9a7d664738b9f95a59
Emeric MUTZIG <emeric.mutzig@etu.unistra.fr>
2020-03-30 12:08:25 +0200

    création d'un fichier de tests pour vérifier le fonctionnement de l'authentification et des rooms

---
+++ Project/ShadowHunter_ClientNetwork_Console/ShadowHunter_ClientNetwork/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using EventSystem;
+using Network.events;
+
+namespace ShadowHunter_ClientNetwork
+{
+    public class Tests
+    {
+        public void LaunchTests()
+        {
+
+            string msg = "";
+            while ((msg = Console.ReadLine()) != "exit")
+            {
+                //client.Send(msg);
+                EventView.Manager.Emit(new ChatMSGEvent() { MSG = msg });
+            }
+        }
+
+        public Tests()
+        {
+        }
+    }
+}
