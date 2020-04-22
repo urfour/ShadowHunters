e0a285cd0c06a7187c9f9c381806e52486b5f989
Romaric Mathis <romaric.mathis@etu.unistra.fr>
2020-04-22 15:40:04 +0200

    gestion des resources pour les images des cartes et des personnages

---
+++ Project/ShadowHunter_Client/Assets/Scripts/GameUI/IconResource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.GameUI
+{
+    [CreateAssetMenu(fileName ="new_icon", menuName ="GameResource/Icon")]
+    class IconResource : ScriptableObject
+    {
+        public string label;
+        public Sprite sprite;
+    }
+}
---
+++ Project/ShadowHunter_Client/Assets/Scripts/GameUI/PlayerBarComponent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBarComponent : MonoBehaviour
+{
+    public PlayerView playerViewPrefab;
+
+    public int NbPlayers = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Init()
+    {
+
+        for (int i = 0; i < NbPlayers; i++)
+        {
+            GameObject p = Instantiate(playerViewPrefab.gameObject, this.transform);
+            PlayerView pv = p.GetComponent<PlayerView>();
+            pv.PlayerId = i;
+            pv.Init();
+        }
+    }
+}
