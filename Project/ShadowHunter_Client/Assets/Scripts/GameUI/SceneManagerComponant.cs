347ebcd7735c1dda5ea92b07f254c46520dc0c46
Romaric Mathis <romaric.mathis@etu.unistra.fr>
2020-04-27 15:25:14 +0200

    implémentation de PlayerViewComponent

---
+++ Project/ShadowHunter_Client/Assets/Scripts/GameUI/SceneManagerComponant.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using Kernel.Settings;
+
+public class SceneManagerComponant : MonoBehaviour
+{
+    //public static SceneManagerComponant Instance { get; set; }
+
+    public static Setting<int> LocalPlayerId = new Setting<int>(0);
+    
+
+}
