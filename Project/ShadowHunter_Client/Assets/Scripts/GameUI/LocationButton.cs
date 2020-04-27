ed480ee3b47dfe4b9651e22c1b4aa920ee265b10
Romaric Mathis <romaric.mathis@etu.unistra.fr>
2020-04-27 20:57:32 +0200

    implémentation de l'affichage du board (disposition des lieux)

---
+++ Project/ShadowHunter_Client/Assets/Scripts/GameUI/LocationButton.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Noyau.Manager.view;
+using EventSystem;
+using UnityEngine.UI;
+
+public class LocationButton : MonoBehaviour
+{
+    //private static Dictionary<Position, Sprite> positionRessourec = new Dictionary<Position, Sprite>();
+    private static string locationPath = "Icons/locations";
+
+
+    private List<(ListenableObject observed, OnNotification notification)> listeners = new List<(ListenableObject observed, OnNotification notification)>();
+
+    public int position;
+    public Position realPosition;
+    public Image image;
+
+    public void AddListeners()
+    {
+        foreach (var (observed, notification) in listeners)
+        {
+            observed.AddListener(notification);
+            notification(observed);
+        }
+    }
+
+    public void RemoveListeners()
+    {
+        foreach (var (observed, notification) in listeners)
+        {
+            observed.RemoveListener(notification);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        realPosition = GameManager.Board[position];
+        image.sprite = Resources.Load<Sprite>(locationPath + "/" + (int)realPosition);
+    }
+    
+}
