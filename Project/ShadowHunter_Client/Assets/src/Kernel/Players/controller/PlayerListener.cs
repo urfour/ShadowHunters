99001674c6e5a8c71e9b78c8942a36a18a362545
MOUNTASIR NASSIME <nassime.mountasir@etu.unistra.fr>
2020-05-02 15:33:54 +0200

    :bug: Correction de Bob partie 3

--- Project/ShadowHunter_Client/Assets/src/Kernel/Players/controller/PlayerListener.cs
+++ Project/ShadowHunter_Client/Assets/src/Kernel/Players/controller/PlayerListener.cs
@@ -412,6 +412,7 @@ namespace Assets.Noyau.Players.controller
                         if (damages >= 2)
                         {
                             // Cas Gatling à considérer quand on aura la foi
+                            damages += playerAttacked.ReductionWounds.Value;
                             playerAttacking.OnAttackingPlayer.Value = playerAttacking.Id;
                             playerAttacked.OnAttackedBy.Value = playerAttacking.Id;
                             playerAttacking.DamageDealed.Value = damages;
