1b3561eb70d44413a6545ca96758a94cf5b4f22c
MOUNTASIR NASSIME <nassime.mountasir@etu.unistra.fr>
2020-05-03 18:24:26 +0200

    :bug: Correction de la vérification du joueur attaquant en cas de décès

--- Project/ShadowHunter_Client/Assets/src/Kernel/Players/model/Player.cs
+++ Project/ShadowHunter_Client/Assets/src/Kernel/Players/model/Player.cs
@@ -295,15 +295,6 @@ public class Player
 
             if (ReductionWounds.Value > 0)
                 damage = (damage - ReductionWounds.Value < 0) ? 0 : damage - ReductionWounds.Value;
-            
-            //si c'est une attaque pour les pouvoirs du Vampire
-            if (isAttack && attacker.Character.characterName == "character.name.vampire")
-                attacker.DamageDealed.Value = damage;
-            
-            this.Wound.Value += damage;
-
-            if (attacker.Character.characterName == "character.name.charles" && isAttack && this.Dead.Value)
-                GameManager.HasKilled.Value = true;
 
             if (isAttack)
             {
@@ -314,6 +305,16 @@ public class Player
             {
                 KernelLog.Instance.DealWounds(this, damage);
             }
+
+            //si c'est une attaque pour les pouvoirs du Vampire
+            if (isAttack && attacker.Character.characterName == "character.name.vampire")
+                attacker.DamageDealed.Value = damage;
+            
+            this.Wound.Value += damage;
+
+            if (attacker.Character.characterName == "character.name.charles" && isAttack && this.Dead.Value)
+                GameManager.HasKilled.Value = true;
+
             return damage;
         }
         return 0;
