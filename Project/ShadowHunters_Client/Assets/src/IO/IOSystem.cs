02f4286c91beb225cdcc8160c7c3726c3bd9bd78
MOUNTASIR NASSIME <nassime.mountasir@etu.unistra.fr>
2020-05-05 16:51:10 +0200

    :art Renommage des dossiers

---
+++ Project/ShadowHunters_Client/Assets/src/IO/IOSystem.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace IO
+{
+    class IOSystem : MonoBehaviour
+    {
+        public static string DataFolder = Path.GetFullPath(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "ShadowHunters"));
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void OnBeforeSceneLoadRuntimeMethod()
+        {
+           
+        }
+
+        public static bool FileExists(string path)
+        {
+            return File.Exists(Path.GetFullPath(Path.Combine(DataFolder, path)));
+        }
+
+        public static bool DirectoryExists(string path)
+        {
+            return Directory.Exists(Path.GetFullPath(Path.Combine(DataFolder, path)));
+        }
+
+        public static void CreateFile(string path, string[] allLines = null, bool deletePrevious = false)
+        {
+            path = Path.GetFullPath(Path.Combine(DataFolder, path));
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
+            if (deletePrevious)
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+            }
+            FileStream fs = File.Create(path);
+            if (allLines != null)
+            {
+                for (int i = 0; i < allLines.Length; i++)
+                {
+                    byte[] b = Encoding.Unicode.GetBytes(allLines[i] + "\n");
+                    fs.Write(b, 0, b.Length);
+                }
+            }
+            fs.Close();
+        }
+
+        public static FileStream CreateFile(string path, bool deletePrevious = false)
+        {
+            path = Path.GetFullPath(Path.Combine(DataFolder, path));
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
+            if (deletePrevious)
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+            }
+            FileStream fs = File.Create(path);
+            return fs;
+        }
+
+        public static void DeleteFile(string path)
+        {
+            path = Path.GetFullPath(Path.Combine(DataFolder, path));
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+
+        public static string GetFullPath(string path)
+        {
+            return Path.GetFullPath(Path.Combine(DataFolder, path));
+        }
+
+        public static string[] GetAllFileNames(string directory, bool with_extension = false)
+        {
+            string[] s = Directory.GetFiles(GetFullPath(directory));
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (with_extension)
+                {
+                    s[i] = Path.GetFileName(s[i]);
+                }
+                else
+                {
+                    s[i] = Path.GetFileNameWithoutExtension(s[i]);
+                }
+            }
+            return s;
+        }
+
+        public static string[] GetAllLines(string path)
+        {
+            path = GetFullPath(path);
+            if (File.Exists(path))
+            {
+                return File.ReadAllLines(path, Encoding.Unicode);
+            }
+            return null;
+        }
+
+        private void Start()
+        {
+            Debug.Log(DataFolder);
+        }
+    }
+}
---
+++ Project/ShadowHunters_Client/Assets/src/ServerInterface/RoomEvents/RoomEvent.cs
@@ -0,0 +1,24 @@
+﻿using Network.events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerInterface.RoomEvents
+{
+    public class RoomEvent : ServerOnlyEvent
+    {
+        public RoomData RoomData { get; set; }
+
+        public RoomEvent(RoomData roomData, string msg = null) : base(msg)
+        {
+            this.RoomData = roomData;
+        }
+
+        public RoomEvent()
+        {
+
+        }
+    }
+}
