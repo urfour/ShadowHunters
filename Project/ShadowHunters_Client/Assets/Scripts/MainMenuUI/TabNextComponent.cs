92e671813caeec813a078caeab70ec65ad000079
MOUNTASIR NASSIME <nassime.mountasir@etu.unistra.fr>
2020-05-18 19:37:04 +0200

    :lipstick: :sparkles: Utilisation de Tab pour naviguer entre les différents items

---
+++ Project/ShadowHunters_Client/Assets/Scripts/MainMenuUI/TabNextComponent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TabNextComponent : MonoBehaviour, IUpdateSelectedHandler
+{
+    public Selectable nextField;
+
+    public void OnUpdateSelected(BaseEventData data)
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+            nextField.Select();
+    }
+}
